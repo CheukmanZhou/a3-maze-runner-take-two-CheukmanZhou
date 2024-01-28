7d13fd219691f1d71b3d20d9e04d0e053cc73319
Cheukman Zhou <cheukmanzhou@gmail.com>
2024-01-28 15:12:10 -0500

    refactored algorithm interface to have getters for canonical and factorized paths close #27

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Algorithm.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Algorithm.java
@@ -2,4 +2,8 @@ package ca.mcmaster.se2aa4.mazerunner;
 
 public interface Algorithm {
     public String navigate();
+
+    public String getCanonicalPath();
+
+    public String getFactorizedPath();
 }
