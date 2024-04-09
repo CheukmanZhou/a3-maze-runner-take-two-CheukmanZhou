265e6e40fb8b18c8145049c1495fc18892e211f7
CheukmanZhou <cheukmanzhou@gmail.com>
2024-04-09 17:54:08 -0400

    removed all files

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Algorithm.java
+++
@@ -1,9 +0,0 @@
-package ca.mcmaster.se2aa4.mazerunner;
-
-public interface Algorithm {
-    public String navigate();
-
-    public String getCanonicalPath();
-
-    public String getFactorizedPath();
-}
