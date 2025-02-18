5437a9fd52b2daafc25a15080a946d849015223a
Cheukman Zhou <cheukmanzhou@gmail.com>
2025-02-18 00:55:04 -0500

    updated and added more tests for Path

--- src/test/java/ca/mcmaster/se2aa4/mazerunner/PathTest.java
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/PathTest.java
@@ -1,36 +1,49 @@
 package ca.mcmaster.se2aa4.mazerunner;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
 
-class PathTest {
+public class PathTest {
     @Test
-    void getCanonicalForm() {
+    void expandFactorizedStringPathTest() {
+        Path path = new Path();
+        assertEquals("FFF RRR LLL", path.expandFactorizedStringPath("3F 3R 3L"));
+    }
+    
+    @Test
+    void getPathStepsTest() {
+        Path path = new Path("FFF");
+        ArrayList<Character> expected = new ArrayList<Character>();
+        expected.add('F');
+        expected.add('F');
+        expected.add('F');
+
+        assertEquals(expected, path.getPathSteps());
+    }
+
+    @Test
+    void addStepTest() {
+        Path path = new Path("F");
+        path.addStep('F');
+        
+        ArrayList<Character> expected = new ArrayList<Character>();
+        expected.add('F');
+        expected.add('F');
+
+        assertEquals(expected, path.getPathSteps());
+    }
+
+    @Test
+    void getCanonicalFormTest() {
         Path path = new Path("FLFFFFFRFFRFFLFFFFFFRFFFFLF");
 
         assertEquals("F L FFFFF R FF R FF L FFFFFF R FFFF L F", path.getCanonicalForm());
     }
 
     @Test
-    void getFactorizedForm() {
+    void getFactorizedFormTest() {
         Path path = new Path("FLFFFFFRFFRFFLFFFFFFRFFFFLF");
 
         assertEquals("F L 5F R 2F R 2F L 6F R 4F L F", path.getFactorizedForm());
-    }
-
-    @Test
-    void expandedPath() {
-        Path path = new Path("4F 3R L");
-
-        assertEquals("FFFF RRR L", path.getCanonicalForm());
-    }
-
-
-    @Test
-    void expandedPath2() {
-        Path path = new Path("10F 11R");
-
-        assertEquals("FFFFFFFFFF RRRRRRRRRRR", path.getCanonicalForm());
     }
 }
