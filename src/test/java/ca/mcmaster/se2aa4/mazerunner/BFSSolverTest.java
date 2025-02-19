4ca16462f60bab70535b287760706319822c5ade
Cheukman Zhou <cheukmanzhou@gmail.com>
2025-02-19 18:20:03 -0500

    updated test for BFS

--- src/test/java/ca/mcmaster/se2aa4/mazerunner/BFSSolverTest.java
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/BFSSolverTest.java
@@ -1,20 +1,53 @@
 package ca.mcmaster.se2aa4.mazerunner;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class BFSSolverTest {
+public class BFSSolverTest {
+    MazeExporter exporter = new Maze2DArrayListExporter();
+    MazeSolver solver = new BFSSolver();
+    
     @Test
-    void solve() throws Exception {
-        String filePath = "./examples/small.maz.txt";
-        MazeExporter exporter = new Maze2DArrayListExporter();
+    void solveStraightMazeTest() throws Exception {
+        String filePath = "./examples/straight.maz.txt";
+        
         Maze maze = exporter.createMaze(filePath);
         
-        MazeSolver solver = new BFSSolver();
+        Path path = solver.solve(maze);
+
+        assertEquals("4F", path.getFactorizedForm());
+    }
+    
+    
+    @Test
+    void solveSmallMazeTest() throws Exception {
+        String filePath = "./examples/small.maz.txt";
+        
+        Maze maze = exporter.createMaze(filePath);
         
         Path path = solver.solve(maze);
 
         assertEquals("F L F R 2F L 6F R 4F R 2F L 2F R 2F L F", path.getFactorizedForm());
+    }
+
+    @Test
+    void solveMediumMazeTest() throws Exception {
+        String filePath = "./examples/medium.maz.txt";
+        
+        Maze maze = exporter.createMaze(filePath);
+        
+        Path path = solver.solve(maze);
+
+        assertEquals("F L 2F R 2F L 18F L 2F R 2F R 8F R 2F L 6F R 10F L 4F R 10F L 10F R 4F L F", path.getFactorizedForm());
+    }
+
+    @Test
+    void solveLargeMazeTest() throws Exception {
+        String filePath = "./examples/large.maz.txt";
+        
+        Maze maze = exporter.createMaze(filePath);
+        
+        Path path = solver.solve(maze);
+
+        assertEquals("15F R 2F L 8F R 2F L 4F R 2F L 4F R 4F L 6F R 2F L 2F R 2F L 2F R 2F L 2F R 2F L 2F R 2F L 2F R 6F L 2F L 2F R F", path.getFactorizedForm());
     }
 }
